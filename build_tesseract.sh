375ae89509cf72060f24a4ae37770a6c58cd2699
Taeyoon Kim <tykimseoul@gmail.com>
2021-01-08 04:09:49 +0900

    update: build script for stability

--- build_tesseract.sh
+++ build_tesseract.sh
@@ -5,21 +5,26 @@ cd ~
 yum install clang -y
 yum install libpng-devel libtiff-devel zlib-devel libwebp-devel libjpeg-turbo-devel -y
 yum install wget -y
-wget https://github.com/DanBloomberg/leptonica/releases/download/1.75.1/leptonica-1.75.1.tar.gz
-tar -xzvf leptonica-1.75.1.tar.gz
-cd leptonica-1.75.1
+wget https://github.com/DanBloomberg/leptonica/releases/download/1.80.0/leptonica-1.80.0.tar.gz
+tar -xzvf leptonica-1.80.0.tar.gz
+cd leptonica-1.80.0
 ./configure && make && make install
+
 cd ~
-wget http://mirror.squ.edu.om/gnu/autoconf-archive/autoconf-archive-2017.09.28.tar.xz
-tar -xvf autoconf-archive-2017.09.28.tar.xz
-cd autoconf-archive-2017.09.28
+wget https://mirror.squ.edu.om/gnu/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
+tar -xvf autoconf-archive-2019.01.06.tar.xz
+cd autoconf-archive-2019.01.06
 ./configure && make && make install
 cp m4/* /usr/share/aclocal/
+
 cd ~
-yum install git-core libtool pkgconfig -y
-git clone --depth 1  https://github.com/tesseract-ocr/tesseract.git tesseract-ocr
-cd tesseract-ocr
+yum install libtool pkgconfig -y
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+mkdir tesseract
+cd tesseract
+wget https://github.com/tesseract-ocr/tesseract/archive/4.1.1.tar.gz
+tar -zxvf 4.1.1.tar.gz
+cd tesseract-4.1.1
 ./autogen.sh
 ./configure
 make
@@ -30,7 +35,7 @@ mkdir tesseract-aws
 cd tesseract-aws
 cp /usr/local/bin/tesseract .
 mkdir lib
-cp /usr/local/lib/libtesseract.so.5 lib/
+cp /usr/local/lib/libtesseract.so.4 lib/
 cp /usr/local/lib/liblept.so.5 lib/
 cp /usr/lib64/libjpeg.so.62 lib/
 cp /usr/lib64/libwebp.so.4 lib/
@@ -44,6 +49,6 @@ wget https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata
 mkdir configs
 cp /usr/local/share/tessdata/configs/pdf configs/
 cp /usr/local/share/tessdata/pdf.ttf .
-cd ..
+
 cp -r /root/tesseract-aws/tessdata/* /usr/local/share/tessdata
 set TESSDATA_PREFIX="/usr/local/share/tessdata"
